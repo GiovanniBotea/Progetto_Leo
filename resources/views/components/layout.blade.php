4be746fdbd52a1e0b4d5f80fe8aad282bdb7301d
Giovanni Botea <giovannibotea@gmail.com>
2023-04-14 18:36:53 +0200

    first scaffolding

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recos</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body>
+
+    <x-navbar />
+
+    <div>
+        {{$slot}}
+    </div>
+
+    <x-footer />
+    
+</body>
+</html>
